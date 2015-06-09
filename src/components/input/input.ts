e69d4aff337b42503c8b94647ea7b5b4fd04deef
Rinat Mullayanov <nutsgamedev@gmail.com>
2015-06-09 16:35:31 +0500

    make the code clearer

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -15,13 +15,12 @@ class InputComponent extends TypedReact.Component<any, any> {
   }
 
   render () {
-    var value = this.state.value;
     // React.DOM provides convenience wrappers around React.createElement for DOM components.
     // https://facebook.github.io/react/docs/top-level-api.html#react.dom
     return (
       React.DOM.div(null,
-        React.DOM.input({type: 'text', value: value, onChange: this.handleChange}),
-        React.DOM.p(null, value)
+        React.DOM.input({type: 'text', value: this.state.value, onChange: this.handleChange}),
+        React.DOM.p(null, this.state.value)
       )
     )
   }
